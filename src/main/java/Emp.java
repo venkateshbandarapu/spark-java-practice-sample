ec7426bb775a84e08582a3078526af1d46459e72
venkateshbandarapu <venki.b321@gmail.com>
2020-02-13 17:04:10 +0530

    added Emp as Encoder for employee data

---
+++ src/main/java/Emp.java
@@ -0,0 +1,32 @@
+public class Emp {
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    int emp_id;
+
+    public int getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(int dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    int dept_id;
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    Double salary;
+}
