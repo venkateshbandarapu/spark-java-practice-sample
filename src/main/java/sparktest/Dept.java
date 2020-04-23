f451ceb7e5fe9fd364a2f10648b9cdb37c16e559
venkateshbandarapu <venki.b321@gmail.com>
2020-04-23 13:49:15 +0530

    removed commented code as per code review

--- src/main/java/sparktest/Dept.java
+++ src/main/java/sparktest/Dept.java
@@ -6,20 +6,4 @@ public class Dept {
     int dept_id;
     String dept_name;
 
-/*    public String getDept_name() {
-        return dept_name;
-    }
-    public int getDept_id() {
-        return dept_id;
-    }
-
-    public void setDept_id(int dept_id) {
-        this.dept_id = dept_id;
-    }
-
-    public void setDept_name(String dept_name) {
-        this.dept_name = dept_name;
-    }*/
-
-
 }
--- src/main/java/sparktest/Emp.java
+++ src/main/java/sparktest/Emp.java
@@ -7,36 +7,5 @@ public class Emp {
     String emp_name;
     Double salary;
 
- /*   public int getEmp_id() {
-        return emp_id;
-    }
-
-    public void setEmp_id(int emp_id) {
-        this.emp_id = emp_id;
-    }
-
-
-    public String getEmp_name() { return emp_name; }
-
-    public void setEmp_name(String emp_name) { this.emp_name = emp_name;}
-
-
-    public int getDept_id() {
-        return dept_id;
-    }
-
-    public void setDept_id(int dept_id) {
-        this.dept_id = dept_id;
-    }
-
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }*/
-
 
 }
