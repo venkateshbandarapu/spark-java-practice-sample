78f80e99380546c1671d702ffd9d06fd251bbc39
venkateshbandarapu <venki.b321@gmail.com>
2023-11-05 22:02:27 +0530

    Update Main.java
    updated code for local run

--- src/main/java/sparktest/Main.java
+++ src/main/java/sparktest/Main.java
@@ -20,6 +20,7 @@ public class Main {
 
         SparkSession spark = SparkSession
                 .builder()
+                .master("local[*]")
                 .enableHiveSupport()
                 .getOrCreate();
 
